b8fe45113c19e8cda900497039ec291166f90e97
huang <1323178720@qq.com>
2019-02-12 00:02:10 +0800

    开始写koa接口

---
+++ components/public/header/topbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-row :gutter="0" class="m-header">
+    <el-col :span="5">
+      <geo/>
+    </el-col>
+    <el-col :span="5">
+      <user/>
+    </el-col>
+    <el-col :span="14">
+      <navbbar/>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from "./geo.vue";
+  import User from './user.vue'
+  import Navbbar from './nav.vue'
+
+  export default {
+    name: "index",
+    components: {Geo,User,Navbbar}
+  }
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/layout.scss";
+  @import "@/assets/css/public/header/index.scss";
+</style>
---
+++ components/public/header/user.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="m-user">
+    <template v-if="user">
+      欢迎您，<sapn class="username">{{user}}</sapn>
+      [<nuxt-link to="/exit">退出</nuxt-link>]
+    </template>
+    <template v-else>
+      <nuxt-link to="/login" class="login">立即登陆</nuxt-link>
+      <nuxt-link to="/register" class="register">注册</nuxt-link>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "user",
+    data() {
+      return {
+        user: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
